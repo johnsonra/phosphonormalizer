ea2840d32daa2026b4067cf01bed68dff671b4c7
Sohrab Saraei <ssaraei@BTK.FI>
2016-11-13 22:58:30 +0200

    pdf vignette, help modified, reformating the code, type check, modify vignette content

--- R/normalizePhospho.R
+++ R/normalizePhospho.R
@@ -1,45 +1,18 @@
-#' Pairwise Normalization of MS-based phosphoproteomic data
-#'
-#' @description  This function compensates for the bias introduced in global 
-#' phosphorylation in the sample after using median normalization.
-#' @param enriched The enriched dataframe with the sequence and modifications 
-#' as the first and the second column in format of charachter, followed by the 
-#' samples (and possibly their technical replicates) in numeric. 
-#' Note that, exactly the same column order in the non-enriched dataset 
-#' must be considered. The dataset is expected to be median normalized.
-#' @param non.enriched The non-enriched dataframe with the sequence and modifications 
-#' as the first and the second column in format of charachter, followed by the samples 
-#' (and possibly their technical replicates) in numeric. Note that, exactly the same 
-#' column order in the enriched dataset must be considered. The dataset is expected to 
-#' be median normalized.
-#' @param phospho a string that shows the term that represents phosphorylation in the 
-#' modification column of the data. If it is not assigned, "Phospho" will be used as 
-#' the default value
-#' @param techRep a factor that holds information about columns order and the technical
-#'  replicates of the samples
-#' @details It is shown that global median normalization can introduce bias in the fold change of global phosphorylation between samples. 
-#' It is suggested that by taking the non-enriched data into consideration, this bias could be compensated(Kauko et al. 2015).
-#' @keywords Phosphoproteomics, Normalization, Mass-spectrometry
-#' @import plyr
-#' @importFrom stats median
-#' @importFrom graphics boxplot
-#' @return A dataframe with the normalize values
-#' @export
-#' @references \url{http://www.nature.com/articles/srep13099}
-#' @examples
-#' #Load the library
-#' library(phosphonormalizer)
-#' #The samples and their technical replicates
-#' techRep <- factor(x = c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5))
-#' #Call the function
-#' norm <- normalizePhospho(enriched.rd, non.enriched.rd, techRep = techRep)
-#' head(norm)
-#' 
+# Pairwise Normalization of MS-based phosphoproteomic data
+# Sohrab Saraei
+# This function compensates for the bias introduced in global 
+# phosphorylation in the sample after using median normalization.
+
 normalizePhospho <- function(enriched, non.enriched, phospho = NULL, techRep)
 {
+    
     stopifnot(!missing(x = enriched))
     stopifnot(!missing(x = non.enriched))
     stopifnot(!missing(x = techRep))
+    stopifnot(class(enriched) == "data.frame")
+    stopifnot(class(non.enriched) == "data.frame")
+    stopifnot(class(phospho) == "character" | phospho == NULL)
+    stopifnot(class(techRep) == "factor" & (length(techRep) == unique(ncol(enriched), ncol(non.enriched)) - 2))
     
     mod = seq = NULL
     
@@ -57,7 +30,7 @@ normalizePhospho <- function(enriched, non.enriched, phospho = NULL, techRep)
     
     if(!all((apply(X = enriched[, -c(1,2)], MARGIN = 2, 
                     function(x) inherits(x = x, what = "numeric")))) &
-       !all((apply(X = non.enriched[, -c(1,2)], MARGIN = 2, 
+        !all((apply(X = non.enriched[, -c(1,2)], MARGIN = 2, 
                     function(x) inherits(x = x, what = "numeric")))))
         stop(paste0("Columns: 3 to ", ncol(enriched), "must be of type numeric!"))
     
@@ -115,7 +88,7 @@ normalizePhospho <- function(enriched, non.enriched, phospho = NULL, techRep)
             max.fc <- as.numeric(
                 unlist(
                     apply(cbind(max.fc, log2(ratios.avg[,i]) - log2(ratios.avg[,j])),
-                          1, max),
+                            1, max),
                     use.names = FALSE)
             )
         }
@@ -129,7 +102,7 @@ normalizePhospho <- function(enriched, non.enriched, phospho = NULL, techRep)
     col.sub <- rowMeans(ratios)
     
     ratios.norm <- as.matrix(apply(ratios, 2, 
-                                   function(x) x - col.sub))
+                                    function(x) x - col.sub))
     
     factors <- 10^(apply(ratios.norm,2,median))
     
